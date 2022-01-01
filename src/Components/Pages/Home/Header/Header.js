16a07e8870eac9698fa0058920ca14d8ac723db5
Karima-Aktari <karimaaktari911@gmail.com>
2022-01-01 16:31:05 +0600

    commit header

--- src/Components/Pages/Home/Header/Header.js
+++ src/Components/Pages/Home/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div className="">
-            <h1>Enjoy Organic Grocery With The Best Quality</h1>
+        <div className="banner">
+            <h1 className="heading text-light">Enjoy Organic Grocery With <br />The Best Quality</h1>
+            <h3 className="text-light">Get your products delivered at your doorsteps all day everyday</h3>
         </div>
     );
 };
--- src/Components/Pages/Home/Home/Home.js
+++ src/Components/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavigationBar from '../../Shared/NavigationBar/NavigationBar';
+import Header from '../Header/Header';
 
 const Home = () => {
     return (
         <div>
             <NavigationBar></NavigationBar>
+            <Header></Header>
             <h1>This is Home</h1>
         </div>
     );
