856a5d3d1de04534fc9f2917df596941433c690b
unknown <freelancerfathe@gmail.com>
2022-01-02 07:34:36 +0600

    'Dashboard'

--- src/components/Dashboard/DashBoardHome/DashboardHome.js
+++ src/components/Dashboard/DashBoardHome/DashboardHome.js
@@ -3,7 +3,7 @@ import React from 'react';
 const DashboardHome = () => {
     return (
         <div>
-            <h2>Dashboard Home</h2>
+            <h2>Dashboard</h2>
         </div>
     );
 };
